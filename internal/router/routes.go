dee067756c12b956348bec3aeb7263002d3477f2
vinicius <niviciusdev@gmail.com>
2024-12-28 22:52:29 -0300

    feat: add more possible routes

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -18,15 +18,25 @@ func initializeRoutes(router *gin.Engine) {
 				"message": "pong1",
 			})
 		})
-		user.GET("/followings", func(c *gin.Context) {
+		user.GET("/following", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"message": "pong2",
+			})
+		})
+		user.GET("/status", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong3",
 			})
 		})
 	}
 
 	group := router.Group("/me")
 	{
+		group.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "General information about logged user",
+			})
+		})
 		group.GET("/followers", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"message": "pong4",
@@ -35,6 +45,26 @@ func initializeRoutes(router *gin.Engine) {
 		group.GET("/following", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"message": "pong5",
+			})
+		})
+		group.GET("/allow-list", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "list of users that are allowed",
+			})
+		})
+		group.POST("/allow-list", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "include of users that are allowed",
+			})
+		})
+		group.GET("/deny-list", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Get list of users that are denied",
+			})
+		})
+		group.POST("/deny-list", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Post Deny List",
 			})
 		})
 	}
